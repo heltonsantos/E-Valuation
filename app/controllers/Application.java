b1a3494b40d75892fac16f0eab009a800fe206ce
heltonsantos <hdas@cin.ufpe.br>
2014-08-18 14:45:22 -0300

    Revert "Junção do back com o front-end"
    This reverts commit 6ab8d0abc851faef212fdf8a4c1aec6eaf03de21.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,57 +1,14 @@
 package controllers;
 
-import models.entities.User;
 import play.*;
-import play.data.Form;
 import play.mvc.*;
+
 import views.html.*;
 
 public class Application extends Controller {
-	
-	
-//	final static Form<User> userForm = form(User.class);
-	
 
     public static Result index() {
-        return ok(index.render("E-Valuation"));
-    }
-    
-    public static Result login() {
-    	return ok(login.render("Login E-Valuation"));
+        return ok(index.render("Your new application is ready."));
     }
 
-    public static Result logon() {
-    	return redirect(routes.Application.home());
-    }
-
-    public static Result signIn() {
-    	return ok(signin.render("Cadastro E-Valuation"));
-    }
-    
-    public static Result home() {
-    	return ok(home.render("E-Valuation"));
-    }
-    
-    public static Result addUser() {
-    	User user = Form.form(User.class).bindFromRequest().get();
-    	user.save();
-    	
-    	return redirect(routes.Application.home());
-    }
-    
-    public static Result pole() {
-    	return ok(enquete.render("Enquete"));
-    }
-    
-    public static Result newPole() {
-    	return ok(novaEnquete.render("Nova Enquete"));
-    }
-    
-    public static Result addPole() {
-    	return TODO;
-    }
-    
-    public static Result myPoles() {
-    	return ok(minhasEnquetes.render("Minhas enquetes"));
-    }
 }
--- build.sbt
+++ build.sbt
@@ -1,4 +1,4 @@
-name := "evaluation"
+name := "e-valuation"
 
 version := "1.0-SNAPSHOT"
 
